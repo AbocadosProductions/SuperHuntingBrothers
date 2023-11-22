098401d8b8ca3885ca7b34a71f1d363202a42000
unknown <josemariadiazdeharo@gmail.com>
2023-11-22 10:35:13 +0100

    Save

--- SuperHuntingBrothersGodot/_Scripts/mouse_controller.gd
+++ SuperHuntingBrothersGodot/_Scripts/mouse_controller.gd
@@ -1,28 +1,25 @@
 extends CharacterBody2D
 
+var move_direction : Vector2
+var wander_time : float
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	randomize_wander()
 
+func _process(delta):
+	if wander_time > 0:
+		wander_time -= delta
+	else:
+		randomize_wander()
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var velocity = move_direction * SPEED
 	move_and_slide()
+
+
+func randomize_wander():
+	move_direction = Vector2(randf_range(-100, 100), randf_range(-100, 100)).normalized()
+	wander_time = randf_range(1, 2)
+	

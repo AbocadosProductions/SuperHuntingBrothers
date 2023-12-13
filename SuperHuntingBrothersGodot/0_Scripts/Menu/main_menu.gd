e546f81724910ffebc6d2b89e52dbca683db46c3
unknown <josemariadiazdeharo@gmail.com>
2023-12-13 20:04:40 +0100

    basic credits

--- SuperHuntingBrothersGodot/0_Scripts/Menu/main_menu.gd
+++ SuperHuntingBrothersGodot/0_Scripts/Menu/main_menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+@export var normal_menu : Node2D
+@export var credits_menu : Node2D
+
 
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file(Constants.FIRST_LEVEL)
@@ -8,8 +11,13 @@ func _on_volumen_slider_value_changed(_value):
 	pass # Replace with call to audio controller
 
 func _on_credits_button_pressed():
-	# get_tree().change_scene_to_file(Constants.CREDITS)
-	pass
+	normal_menu.set_visible(false)
+	credits_menu.set_visible(true)
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_back_to_menu_button_pressed():
+	normal_menu.set_visible(true)
+	credits_menu.set_visible(false)

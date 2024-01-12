92e865ced89c51011f7c07094328abfb1eb07f3b
Juanjo <juanjogarridobascaran@gmail.com>
2024-01-12 15:04:39 +0100

    Finished tutorial node

--- SuperHuntingBrothersGodot/0_Scripts/Menu/tutorial_menu.gd
+++ SuperHuntingBrothersGodot/0_Scripts/Menu/tutorial_menu.gd
@@ -1,10 +1,14 @@
 extends Control
 
-@export var first_block : Node2D
-@export var second_block : Node2D
+@export var first_block : Panel
+@export var second_block : Panel
 @export var next_button : Button
 @export var menu_button : Button
 @export var play_button : Button
+@onready var RatonNormalAnim : AnimatedSprite2D = $Panel/first_to_show/RatonNormal
+@onready var RatonCorriendoAnim : AnimatedSprite2D = $Panel/first_to_show/RatonCorriendo
+@onready var CattanitaAnim : AnimatedSprite2D = $Panel/second_to_show/Cattanita
+@onready var AguacateAnim : AnimatedSprite2D = $Panel/second_to_show/Aguacate
 
 var showing_first_block = true
 var moving_blocks = false
@@ -13,17 +17,21 @@ var moving_blocks = false
 func _ready():
 	menu_button.set_visible(false)
 	play_button.set_visible(false)
+	RatonNormalAnim.play()
+	RatonCorriendoAnim.play()	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if moving_blocks:
-		first_block.position[1] -= 10
-		second_block.position[1] -= 10
-		if second_block.position[1] == -80:
+		first_block.position[1] -= 3
+		second_block.position[1] -= 3
+		if second_block.position[1] == 0:
 			moving_blocks = false
 			showing_first_block = false
 			menu_button.set_visible(true)
 			play_button.set_visible(true)
+			CattanitaAnim.play()
+			AguacateAnim.play()
 
 func _on_next_button_pressed():
 	if showing_first_block:

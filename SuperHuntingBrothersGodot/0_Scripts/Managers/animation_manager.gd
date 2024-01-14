29739630fade32d0d10bb2969e991b31cd2ed81c
unknown <josemariadiazdeharo@gmail.com>
2024-01-14 11:29:13 +0100

    Created fancy tutorial

--- SuperHuntingBrothersGodot/0_Scripts/Managers/animation_manager.gd
+++ SuperHuntingBrothersGodot/0_Scripts/Managers/animation_manager.gd
@@ -4,7 +4,6 @@ extends Node
 @export var emisioner : Node2D
 var animation_list : Array
 
-
 func _ready():
 	animation_list = animation_player.get_animation_list()
 	emisioner.External_Signal.connect(on_signal_detected)
@@ -13,5 +12,4 @@ func on_signal_detected(signal_data):
 	if signal_data != Constants.ANIMATION_BEGGINING_RIGHT:
 		signal_data += emisioner.direction
 	if signal_data in animation_list:
-
 		animation_player.play(signal_data)
